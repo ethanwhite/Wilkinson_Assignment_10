d73987eebcf75e71f56ba3b0a5a24dbf005c0e38
Krystan Wilkinson <krystan@mote.org>
2016-11-18 12:51:28 -0500

    Create a new file for your analysis named houseelf-analysis.R and add a comment at the top describing what the analysis is intended to do

---
+++ houseelf-analysis.R
@@ -0,0 +1,24 @@
+##########################################################################################
+
+#                                   ASSIGNMENT                                           #
+
+##########################################################################################
+
+
+
+#--- Exercise 1 ---#
+
+#Set up git
+
+
+
+
+#--- Exercise 2 ---#
+#We needed to create a new .R script for Dr. Granger's analysis
+
+#Dr. Granger is interested in studying the relationship between the length of house-elves’ 
+#ears and aspects of their DNA.
+
+
+
+
