b343add5c2241821ec17dc6a2901e4996dcc3173
Krystan Wilkinson <krystan@mote.org>
2016-11-18 13:23:47 -0500

    Add function to calculate GC content of a sequence

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -43,3 +43,21 @@ elf.data<-read.csv("data/houseelf-earlength-dna-data.csv_1", sep=",", stringsAsF
 #--- Exercise 5 ---#
 
 #Connect your local git repository to your remote repository on Github.
+
+
+
+
+#--- Exercise 6 ---#
+
+#calculate the GC-content of a sequence, regardless of the capitalization of that sequence
+get_gc_content<-function(dna.seq){
+  gs <- str_count(dna.seq, "g")
+  Gs <- str_count(dna.seq, "G")
+  cs <- str_count(dna.seq, "c")
+  Cs <- str_count(dna.seq, "C")
+  gc_content <- (Gs + Cs + gs + cs) / str_length(dna.seq) * 100 
+  return(gc_content)
+}
+
+
+#Commit and push this change to GitHub
